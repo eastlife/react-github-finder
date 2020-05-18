3154477e05d5f5b553e55910caf2713496926994
eastlife <dengbx1212@163.com>
2020-05-17 23:19:47 -0400

    add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Fragment, Component } from 'react';
+import Navbar from './component/layout/Navbar'
 import './App.css';
 
 // class-based component
 class App extends Component {
   // life cycle method that runs at a certain point when the components are loaded
   render() {
+    const name = 'John Doe';
+
     return (
       // has to have one parent element
-      <Fragment>
-        <h1>Hello from React</h1>
-        <h2>Goodbye</h2>
-      </Fragment>
+      <div className='App'>
+        <Navbar title="Github Finder" icon="fab fa-github"/>
+      </div>
     );
     // An alternative of javascipt approach
     // return React.createElement('div', { className: 'App' }, 
