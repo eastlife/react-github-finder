e945c50ffdbb32ea7477957636db5b504a93414e
eastlife <dengbx1212@163.com>
2020-05-18 16:29:36 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
 import Navbar from './component/layout/Navbar';
 import UserItem from './component/users/Useritem';
 import './App.css';
@@ -7,8 +7,6 @@ import './App.css';
 class App extends Component {
   // life cycle method that runs at a certain point when the components are loaded
   render() {
-    const name = 'John Doe';
-
     return (
       // has to have one parent element
       <div className='App'>
