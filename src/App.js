dd5ada6fd798d0991cc62777b5059813db5759da
eastlife <dengbx1212@163.com>
2020-05-29 00:22:04 -0400

    home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import Navbar from './component/layout/Navbar';
-import Users from './component/users/Users';
 import User from './component/users/User';
-import Search from './component/users/Search';
 import Alert from './component/layout/Alert';
+import Home from './component/pages/Home';
 import About from './component/pages/About';
 import './App.css';
 
@@ -22,12 +21,7 @@ const App = () => {
             <div className="container">
               <Alert alert={alert} />
               <Switch>
-                <Route exact path='/' render={props => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )} />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
               </Switch>
